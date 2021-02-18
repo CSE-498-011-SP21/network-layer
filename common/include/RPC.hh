4cf8cb04bc14eb1ca0646b49d61929552a886bfa
dePaul Miller <dsm220@lehigh.edu>
2021-02-18 10:58:14 -0500

    Initial commit

---
+++ common/include/RPC.hh
@@ -0,0 +1,43 @@
+#include <functional>
+#include <boost/asio.hpp>
+#include <iostream>
+
+#ifndef NETWORKLAYER_RPC_HH
+#define NETWORKLAYER_RPC_HH
+
+namespace cse498 {
+
+    using pack_t = std::vector<char>;
+
+    class RPC {
+    public:
+        RPC() {}
+
+        virtual ~RPC() {}
+
+        /**
+         * Register RPC with an id number and a function taking in pack_t and returning a pack_t
+         * @param fnId id number
+         * @param fn RPC function
+         */
+        virtual void registerRPC(uint64_t fnId, std::function<pack_t(pack_t)> fn) = 0;
+    };
+
+    class RPClient {
+    public:
+        RPClient() {}
+
+        virtual ~RPClient() {}
+
+        /**
+         * Call remote function by sending data
+         * @param fnID RPC id number
+         * @param data data to send
+         * @return pack_t returned by remote function
+         */
+        virtual pack_t callRemote(uint64_t fnID, pack_t data) = 0;
+    };
+
+}
+
+#endif //NETWORKLAYER_RPC_HH
---
+++ common/src/fabrics_test.cc
@@ -0,0 +1,40 @@
+#include <networklayer/config.hh>
+#include <rdma/fabric.h>
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+    std::cout << "Using Network version: " << NETWORK_VER << std::endl;
+
+    std::cout << "Using Fabric version : " << FI_MAJOR_VERSION << ":" << FI_MINOR_VERSION << std::endl;
+
+    std::cout << std::endl;
+
+    const int version = FI_VERSION(FI_MAJOR_VERSION, FI_MINOR_VERSION);
+
+    fi_info* info;
+
+    const char* node = nullptr;
+
+    if(fi_getinfo(version, node, nullptr, 0, nullptr, &info) != 0){
+        return 1;
+    }
+
+    fi_info* start = info;
+
+    while(start){
+        std::cout << start->fabric_attr->name << std::endl;
+        std::cout << (start->caps & FI_ATOMIC ? "FI_ATOMIC" : "NO FI_ATOMIC") << std::endl;
+        std::cout << (start->caps & FI_FENCE ? "FI_FENCE" : "NO FI_FENCE") << std::endl;
+        std::cout << (start->caps & FI_MSG ? "FI_MSG" : "NO FI_MSG") << std::endl;
+        std::cout << (start->caps & FI_READ ? "FI_READ" : "NO FI_READ") << std::endl;
+        std::cout << (start->caps & FI_RMA ? "FI_RMA" : "NO FI_RMA") << std::endl;
+
+        std::cout << std::endl;
+        start = start->next;
+    }
+
+    fi_freeinfo(info);
+
+    return 0;
+}
