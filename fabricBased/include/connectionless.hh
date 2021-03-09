a84be26ba3d6a0c050f423948ba242bc11d86b3d
dePaul Miller <dsm220@lehigh.edu>
2021-03-09 15:29:00 -0500

    Updated code; added connectionless.hh

--- fabricBased/include/connectionless.hh
+++ fabricBased/include/connectionless.hh
@@ -1,10 +1,7 @@
 //
-// Created by depaulsmiller on 2/18/21.
+// Created by depaulsmiller on 3/9/21.
 //
 
-#pragma once
-
-//#include <boost/asio.hpp>
 #include <networklayer/RPC.hh>
 #include <tbb/concurrent_unordered_map.h>
 #include <cassert>
@@ -21,93 +18,25 @@
 
 #include <spdlog/spdlog.h>
 
-#define ERRCHK(x) error_check((x), __FILE__, __LINE__);
+#ifndef NETWORKLAYER_CONNECTIONLESS_HH
+#define NETWORKLAYER_CONNECTIONLESS_HH
 
-inline void error_check(int err, std::string file, int line) {
+#define ERRCHK(x) error_check_2((x), __FILE__, __LINE__);
+
+inline void error_check_2(int err, std::string file, int line) {
     if (err) {
         SPDLOG_CRITICAL("ERROR ({0}): {1} {2}:{3}", err, fi_strerror(-err), file, line);
         _exit(1);
     }
 }
 
-static_assert(FI_MAJOR_VERSION == 1 && FI_MINOR_VERSION == 6, "Rely on libfabric 1.6");
-
 namespace cse498 {
 
-    /**
-     * Default port for RPCs
-     */
-    int DEFAULT_PORT = 8080;
-
-    /*
-     * Protocol:
-     * Client Sends:
-     * uint64_t addrlen
-     * bytes of addr
-     * Header
-     * bytes of payload
-     *
-     * Server Sends:
-     * uint64_t size of payload
-     * bytes of payload
-     */
-
-    /**
-     * Header for RPC
-     */
-    struct Header {
-        /**
-         * Function ID number
-         */
-        uint64_t fnID;
-        /**
-         * Size of argument to be sent
-         */
-        uint64_t sizeOfArg;
-    };
-
-    /**
-     * Waits for something to happen in the cq.
-     * @param cq The CQ we are waiting on
-     * @return error
-     */
-    int wait_for_completion(struct fid_cq *cq) {
-        fi_cq_entry entry;
-        int ret;
-        while (1) {
-            ret = fi_cq_read(cq, &entry, 1);
-            if (ret > 0) return 0;
-            if (ret != -FI_EAGAIN) {
-                // New error on queue
-                struct fi_cq_err_entry err_entry;
-                fi_cq_readerr(cq, &err_entry, 0);
-                SPDLOG_WARN("{0} {1}", fi_strerror(err_entry.err),
-                            fi_cq_strerror(cq, err_entry.prov_errno,
-                                           err_entry.err_data, NULL, 0));
-                return ret;
-            }
-        }
-    }
-
-
-    /**
-     * FabricRPC class. Currently a connectionless server.
-     */
-    class FabricRPC final : public RPC {
+    class ConnectionlessServer {
     public:
-        /**
-         * Create server. Default mapping of function 0 to shutdown.
-         * @param fabricAddress Utilize this address
-         */
-        FabricRPC(const char *fabricAddress) : fnMap(
-                new tbb::concurrent_unordered_map<uint64_t, std::function<pack_t(pack_t)>>()) {
+        ConnectionlessServer(const char *fabricAddress) {
 
             done = false;
-
-            assert(fnMap->insert({0, [this](pack_t p) {
-                done = true;
-                return p;
-            }}).second);
 
             SPDLOG_TRACE("Getting fi provider");
             hints = fi_allocinfo();
@@ -147,12 +76,6 @@ namespace cse498 {
 
             // Could create multiple endpoints, especially if there are multiple NICs available.
 
-            // malloc buffers
-            local_buf = new char[max_msg_size];
-            remote_buf = new char[max_msg_size];
-
-            memset(remote_buf, 0, max_msg_size);
-
             ERRCHK(fi_ep_bind(ep, &av->fid, 0));
 
             SPDLOG_TRACE("Binding Rx CQ to EP");
@@ -166,21 +89,15 @@ namespace cse498 {
             SPDLOG_TRACE("Enabling EP");
             ERRCHK(fi_enable(ep));
 
-            // Register memory region for RDMA
-            SPDLOG_TRACE("Registering memory region");
-            ERRCHK(fi_mr_reg(domain, remote_buf, max_msg_size,
-                             FI_WRITE | FI_REMOTE_WRITE | FI_READ | FI_REMOTE_READ, 0,
-                             0, 0, &mr, NULL));
         }
 
         /**
          * Destructor
          */
-        ~FabricRPC() {
+        ~ConnectionlessServer() {
             fi_freeinfo(hints);
             fi_freeinfo(fi);
 
-            ERRCHK(fi_close(&mr->fid));
             ERRCHK(fi_close(&ep->fid));
             ERRCHK(fi_close(&tx_cq->fid));
             ERRCHK(fi_close(&rx_cq->fid));
@@ -188,93 +105,81 @@ namespace cse498 {
             ERRCHK(fi_close(&fabric->fid));
             ERRCHK(fi_close(&domain->fid));
 
-            delete[] local_buf;
-            delete[] remote_buf;
             SPDLOG_INFO("All freed");
         }
 
-        /**
-         * Register RPC with an id number and a function taking in pack_t and returning a pack_t
-         * @param fnID id number
-         * @param fn RPC function
-         */
-        void registerRPC(uint64_t fnID, std::function<pack_t(pack_t)> fn) {
-            assert(fnMap->insert({fnID, fn}).second);
+        void recv_addr(char *buf, size_t size, fi_addr_t &remote_addr) {
+            ERRCHK(fi_recv(ep, buf, size, nullptr, 0, nullptr));
+            ERRCHK(wait_for_completion(rx_cq));
+            uint64_t sizeOfAddress = *(uint64_t *) remote_buf;
+
+            SPDLOG_TRACE("Server: Adding client to AV");
+
+            if (1 != fi_av_insert(av, remote_buf + sizeof(uint64_t), 1, &remote_addr, 0, NULL)) {
+                std::cerr << "ERROR - fi_av_insert did not return 1" << std::endl;
+                perror("Error");
+                exit(1);
+            }
         }
 
-        /**
-         * Start the server.
-         */
-        void start() {
-            while (!done) {
-                SPDLOG_TRACE("Server: Receiving client address");
+        void recv(fi_addr_t remote_addr, char *buf, size_t size) {
+            ERRCHK(fi_recv(ep, buf, size, nullptr, remote_addr, nullptr));
+            ERRCHK(wait_for_completion(rx_cq));
+        }
 
-                ERRCHK(fi_recv(ep, remote_buf, max_msg_size, nullptr, 0, nullptr));
-                ERRCHK(wait_for_completion(rx_cq));
-                SPDLOG_TRACE("Completion");
+        void send(fi_addr_t remote_addr, char *buf, size_t size) {
+            ERRCHK(fi_send(ep, local_buf, size, nullptr, remote_addr, nullptr));
+            ERRCHK(wait_for_completion(tx_cq));
+        }
 
-                uint64_t sizeOfAddress = *(uint64_t *) remote_buf;
-
-                fi_addr_t remote_addr;
-                SPDLOG_TRACE("Server: Adding client to AV");
-
-                if (1 != fi_av_insert(av, remote_buf + sizeof(uint64_t), 1, &remote_addr, 0, NULL)) {
-                    std::cerr << "ERROR - fi_av_insert did not return 1" << std::endl;
-                    perror("Error");
-                    exit(1);
-                }
-
-                Header h = *(Header *) (remote_buf + sizeof(uint64_t) + sizeOfAddress);
-                auto fnRes = fnMap->find(h.fnID);
-
-                pack_t p(h.sizeOfArg);
-
-                memcpy(p.data(), remote_buf + sizeof(uint64_t) + sizeOfAddress + sizeof(Header), h.sizeOfArg);
-
-                auto res = fnRes->second(p);
-
-                uint64_t size = res.size();
-
-                memcpy(local_buf, (char *) &size, sizeof(uint64_t));
-                memcpy(local_buf + sizeof(uint64_t), res.data(), size);
-
-                ERRCHK(fi_send(ep, local_buf, res.size() + sizeof(uint64_t), nullptr, remote_addr, nullptr));
-                ERRCHK(wait_for_completion(tx_cq));
-                ERRCHK(fi_av_remove(av, &remote_addr, 1, 0));
-            }
+        void registerMR(char* buf, size_t size, fid_mr& mr){
+            ERRCHK(fi_mr_reg(domain, buf, size,
+                             FI_WRITE | FI_REMOTE_WRITE | FI_READ | FI_REMOTE_READ, 0,
+                             0, 0, &mr, NULL));
         }
 
     private:
 
-
-        tbb::concurrent_unordered_map<uint64_t, std::function<pack_t(pack_t)>> *fnMap;
+        int wait_for_completion(struct fid_cq *cq) {
+            fi_cq_entry entry;
+            int ret;
+            while (1) {
+                ret = fi_cq_read(cq, &entry, 1);
+                if (ret > 0) return 0;
+                if (ret != -FI_EAGAIN) {
+                    // New error on queue
+                    struct fi_cq_err_entry err_entry;
+                    fi_cq_readerr(cq, &err_entry, 0);
+                    SPDLOG_WARN("{0} {1}", fi_strerror(err_entry.err),
+                                fi_cq_strerror(cq, err_entry.prov_errno,
+                                               err_entry.err_data, NULL, 0));
+                    return ret;
+                }
+            }
+        }
 
         fi_info *fi, *hints;
         fid_fabric *fabric;
-        fid_domain *domain;
         fi_cq_attr cq_attr;
         fi_av_attr av_attr;
         fid_av *av;
         fid_cq *tx_cq, *rx_cq;
         fid_ep *ep;
         size_t max_msg_size = 4096;
-        fid_mr *mr;
-        char *local_buf;
-        char *remote_buf;
         std::atomic_bool done;
     };
 
     /**
      * RPC client using libfabric
      */
-    class FabricRPClient final : public RPClient {
+    class ConnectionlessClient {
     public:
         /**
          * Create client
          * @param address connect to this address
          * @param port connect to this port
          */
-        FabricRPClient(const std::string &address, uint16_t port) {
+        ConnectionlessClient(const std::string &address, uint16_t port) {
             SPDLOG_TRACE("Getting fi provider");
             hints = fi_allocinfo();
             hints->caps = FI_MSG;
@@ -309,12 +214,6 @@ namespace cse498 {
 
             // Could create multiple endpoints, especially if there are multiple NICs available.
 
-            // malloc buffers
-            remote_buf = new char[max_msg_size];
-            local_buf = new char[max_msg_size];
-
-            memset(remote_buf, 0, max_msg_size);
-
             ERRCHK(fi_ep_bind(ep, &av->fid, 0));
             SPDLOG_TRACE("Binding TX CQ to EP");
             ERRCHK(fi_ep_bind(ep, &tx_cq->fid, FI_TRANSMIT));
@@ -325,12 +224,6 @@ namespace cse498 {
             SPDLOG_TRACE("Enabling EP");
             ERRCHK(fi_enable(ep));
 
-            // Register memory region for RDMA
-            SPDLOG_TRACE("Registering memory region");
-            ERRCHK(fi_mr_reg(domain, remote_buf, max_msg_size,
-                             FI_WRITE | FI_REMOTE_WRITE | FI_READ | FI_REMOTE_READ, 0,
-                             0, 0, &mr, NULL));
-
             if (1 != fi_av_insert(av, fi->dest_addr, 1, &remote_addr, 0, NULL)) {
                 exit(1);
             }
@@ -340,7 +233,7 @@ namespace cse498 {
         /**
          * Destructor
          */
-        ~FabricRPClient() {
+        ~ConnectionlessClient() {
             fi_freeinfo(hints);
             fi_freeinfo(fi);
 
@@ -352,20 +245,10 @@ namespace cse498 {
             ERRCHK(fi_close(&fabric->fid));
             ERRCHK(fi_close(&domain->fid));
 
-            delete[] local_buf;
-            delete[] remote_buf;
         }
 
-        /**
-         * Call remote function by sending data
-         * @param fnID RPC id number
-         * @param data data to send
-         * @return pack_t returned by remote function
-         */
-        pack_t callRemote(uint64_t fnID, pack_t data) {
 
-            assert(sizeof(size_t) == sizeof(uint64_t));
-
+        void send_addr(char *buf, size_t size) {
             size_t addrlen = 0;
             fi_getname(&ep->fid, nullptr, &addrlen);
             char *addr = new char[addrlen];
@@ -373,36 +256,53 @@ namespace cse498 {
 
             SPDLOG_DEBUG("Client: Sending ({0}) {1} to {2}", addrlen, (void *) addr, remote_addr);
 
-            memcpy(local_buf, &addrlen, sizeof(uint64_t));
-            memcpy(local_buf + sizeof(uint64_t), addr, addrlen);
+            memcpy(buf, &addrlen, sizeof(uint64_t));
+            memcpy(buf + sizeof(uint64_t), addr, addrlen);
+            assert(size <= sizeof(uint64_t) + addrlen);
 
-            Header h;
-            h.sizeOfArg = data.size();
-            h.fnID = fnID;
-            memcpy(local_buf + sizeof(uint64_t) + addrlen, (char *) &h, sizeof(Header));
-            if (sizeof(uint64_t) + addrlen + data.size() + sizeof(Header) > max_msg_size) {
-                exit(1);
-            }
-            memcpy(local_buf + sizeof(uint64_t) + addrlen + sizeof(Header), data.data(), data.size());
-
-            ERRCHK(fi_send(ep, local_buf, data.size() + sizeof(uint64_t) + addrlen + data.size() + sizeof(Header), nullptr, remote_addr, nullptr));
+            ERRCHK(fi_send(ep, buf, buf + sizeof(uint64_t) + addrlen, nullptr, remote_addr, nullptr));
             ERRCHK(wait_for_completion(tx_cq));
+        }
 
-            ERRCHK(fi_recv(ep, remote_buf, max_msg_size, nullptr, 0, nullptr));
+        void recv(char *buf, size_t size) {
+            ERRCHK(fi_recv(ep, buf, size, nullptr, remote_addr, nullptr));
             ERRCHK(wait_for_completion(rx_cq));
+        }
 
-            uint64_t size = *(uint64_t *) remote_buf;
+        void send(char *buf, size_t size) {
+            ERRCHK(fi_send(ep, local_buf, size, nullptr, remote_addr, nullptr));
+            ERRCHK(wait_for_completion(tx_cq));
+        }
 
-            pack_t p(remote_buf + sizeof(uint64_t), remote_buf + sizeof(uint64_t) + size);
-
-            return p;
+        void registerMR(char* buf, size_t size, fid_mr& mr){
+            ERRCHK(fi_mr_reg(domain, buf, size,
+                             FI_WRITE | FI_REMOTE_WRITE | FI_READ | FI_REMOTE_READ, 0,
+                             0, 0, &mr, NULL));
         }
 
     private:
+
+        int wait_for_completion(struct fid_cq *cq) {
+            fi_cq_entry entry;
+            int ret;
+            while (1) {
+                ret = fi_cq_read(cq, &entry, 1);
+                if (ret > 0) return 0;
+                if (ret != -FI_EAGAIN) {
+                    // New error on queue
+                    struct fi_cq_err_entry err_entry;
+                    fi_cq_readerr(cq, &err_entry, 0);
+                    SPDLOG_WARN("{0} {1}", fi_strerror(err_entry.err),
+                                fi_cq_strerror(cq, err_entry.prov_errno,
+                                               err_entry.err_data, NULL, 0));
+                    return ret;
+                }
+            }
+        }
+
         fi_addr_t remote_addr;
         fi_info *fi, *hints;
         fid_fabric *fabric;
-        fid_domain *domain;
         fi_cq_attr cq_attr;
         fi_av_attr av_attr;
         fid_av *av;
@@ -410,8 +310,8 @@ namespace cse498 {
         fid_ep *ep;
         size_t max_msg_size = 4096;
         fid_mr *mr;
-        char *local_buf;
-        char *remote_buf;
-
     };
 }
+
+
+#endif //NETWORKLAYER_CONNECTIONLESS_HH
