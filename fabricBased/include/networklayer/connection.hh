bcac1b0779e05ab213be1276b7bfdfa7bc375386
jimjbrettj <jdj221@lehigh.edu>
2021-03-20 18:58:40 -0400

    non-blocking read/recv TODO/Test

--- fabricBased/include/networklayer/connection.hh
+++ fabricBased/include/networklayer/connection.hh
@@ -28,7 +28,7 @@ inline int callCheck(int err, const char *file, int line, bool abort = true) {
     return err;
 }
 
-#define ERRREPORT(x) error_report2((x), __FILE__, __LINE__);
+#define ERRREPORT2(x) error_report2((x), __FILE__, __LINE__);
 
 inline bool error_report2(int err, std::string file, int line) {
     if (err) {
@@ -181,13 +181,14 @@ namespace cse498 {
          * @param data The data to send
          * @param size The size of the data
          **/
-        inline void async_send(const char *data, const size_t size) {
+        inline bool async_send(const char *data, const size_t size) {
             if (size > MAX_MSG_SIZE) {
                 LOG2<ERROR>() << "Too large of a message!";
-                exit(1);
+                return false;
             }
             ++msg_sends;
-            SAFE_CALL(fi_send(ep, data, size, nullptr, 0, nullptr));
+            //SAFE_CALL(fi_send(ep, data, size, nullptr, 0, nullptr));
+            return ERRREPORT2(fi_send(ep, data, size, nullptr, 0, nullptr));
         }
 
         /**
@@ -211,6 +212,22 @@ namespace cse498 {
             SAFE_CALL(fi_recv(ep, buf, max_len, nullptr, 0, nullptr));
             LOG2<DEBUG3>() << "Receiving up to " << max_len << " bytes";
             SAFE_CALL(wait_for_completion(rx_cq));
+        }
+
+        /**
+         * Same as above but nonblocking.
+         *
+         * @param buf The buffer to store the message data in
+         * @param max_len The maximum length of the message (should be <= MAX_MSG_SIZE)
+         **/
+        inline bool try_recv(char *buf, size_t max_len) {
+            LOG2<DEBUG3>() << "Receiving up to " << max_len << " bytes";
+            bool b = ERRREPORT2(fi_recv(ep, buf, max_len, nullptr, 0, nullptr));
+            if (b) {
+                SAFE_CALL(wait_for_completion(rx_cq));
+                return true;
+            }
+            return false;
         }
 
         /**
@@ -263,6 +280,24 @@ namespace cse498 {
             SAFE_CALL(fi_read(ep, buf, size, nullptr, 0, addr, key, nullptr));
             LOG2<DEBUG3>() << "Read " << key << "-" << addr << " sent";
             SAFE_CALL(wait_for_completion(tx_cq));
+        }
+
+        /**
+         * Same as above but nonblocking
+         * @param buf
+         * @param size
+         * @param addr
+         * @param key
+         */
+        inline bool try_read(char *buf, size_t size, uint64_t addr, uint64_t key) {
+            //SAFE_CALL(fi_read(ep, buf, size, nullptr, 0, addr, key, nullptr));
+            LOG2<DEBUG3>() << "Read " << key << "-" << addr << " sent";
+            bool b = ERRREPORT2(fi_read(ep, buf, size, nullptr, 0, addr, key, nullptr));
+            if (b) {
+                SAFE_CALL(wait_for_completion(tx_cq));
+                return true;
+            }
+            return false;
         }
 
         /*inline void read_local(char *buf, size_t size, uint64_t addr, uint64_t key) {
