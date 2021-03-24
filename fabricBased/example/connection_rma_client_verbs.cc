34543cd905058b82c128a9ea3aedaa5cb98ca0dc
depaulm <depaulm@localhost>
2021-03-24 13:01:56 -0600

    Update

--- fabricBased/example/connection_rma_client_verbs.cc
+++ fabricBased/example/connection_rma_client_verbs.cc
@@ -23,11 +23,13 @@ int main(int argc, char** argv){
 
     std::cerr << "Recv" << std::endl;
 
-    c2->recv(buf, 1);
+    c2->recv(buf, sizeof(uint64_t));
+
+    uint64_t remoteKey = *((uint64_t *) buf.get());
 
     *((uint64_t *) buf.get()) = 10;
 
-    c2->read(buf, sizeof(uint64_t), 0, 1);
+    c2->read(buf, sizeof(uint64_t), 0, remoteKey);
 
     while ((*(uint64_t *) buf.get()) != ~0);
 
